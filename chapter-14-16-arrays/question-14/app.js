7fe72b05aae03a8a75d8f706fc67f1de4ac12f7a
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-01 13:53:30 +0500

    Add question-14: Array pop method and fix question-13 semicolons

---
+++ chapter-14-16-arrays/question-14/app.js
@@ -0,0 +1,10 @@
+var arr = ["keyboard", "mouse", "printer", "monitor"];
+
+document.writeln(`Out: <br />`);
+document.writeln(`${arr.pop()} <br />`);
+document.writeln(`Out: <br />`);
+document.writeln(`${arr.pop()} <br />`);
+document.writeln(`Out: <br />`);
+document.writeln(`${arr.pop()} <br />`);
+document.writeln(`Out: <br />`);
+document.writeln(`${arr.pop()} <br />`);
