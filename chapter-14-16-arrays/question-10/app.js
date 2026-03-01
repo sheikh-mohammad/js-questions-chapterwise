2dc3880dfa0e75bbe73b522b56ecca2c1b0259b2
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-01 07:59:18 +0500

    Add question-10: Array length and manipulation

---
+++ chapter-14-16-arrays/question-10/app.js
@@ -0,0 +1,6 @@
+var studentScores = [320, 230, 480, 120];
+
+var orderedScores = studentScores.toSorted();
+
+document.writeln(`Scores of Students: ${studentScores} <br />`);
+document.writeln(`Ordered Scores of Students: ${orderedScores}`);
