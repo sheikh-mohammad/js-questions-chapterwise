737e1e2e42053d30e31cb8db83d05d7b9642add5
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-01 13:52:18 +0500

    Add question-13: Array reverse and pop methods

---
+++ chapter-14-16-arrays/question-13/app.js
@@ -0,0 +1,10 @@
+var arr = ["keyboard", "mouse", "printer", "monitor"].reverse();
+
+document.writeln(`Out: <br />`)
+document.writeln(`${arr.pop()} <br />`)
+document.writeln(`Out: <br />`)
+document.writeln(`${arr.pop()} <br />`)
+document.writeln(`Out: <br />`)
+document.writeln(`${arr.pop()} <br />`)
+document.writeln(`Out: <br />`)
+document.writeln(`${arr.pop()} <br />`)
