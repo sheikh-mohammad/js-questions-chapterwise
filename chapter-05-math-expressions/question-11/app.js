d712e3cd8a3238354e5267ab2f4ab99e66297d79
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-02 15:48:40 +0500

    Add question-11: Math expressions

--- chapter-05-math-expressions/question-11/app.js
+++ chapter-05-math-expressions/question-11/app.js
@@ -1,1 +1,8 @@
-// app.js for question-11 
+var currentYear = 2026;
+var birthYear = 2011;
+
+var age = currentYear - birthYear
+
+document.writeln(`Current Year: ${currentYear} <br />`);
+document.writeln(`Birth Year: ${birthYear} <br />`);
+document.writeln(`Your Age is: ${age} <br />`)
