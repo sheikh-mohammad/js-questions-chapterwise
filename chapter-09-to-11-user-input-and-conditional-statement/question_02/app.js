0ec9e42045cd3ca6da2d23b3b1f164b6640ec8df
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-06 14:44:23 +0500

    Add solution to question 01 - city name prompt and conditional welcome

--- chapter-09-to-11-user-input-and-conditional-statement/question_02/app.js
+++ chapter-09-to-11-user-input-and-conditional-statement/question_02/app.js
@@ -1,1 +1,5 @@
 // Question 2 - Chapter 09-11 User Input and Conditional Statement
+
+// 2. Write a program to take “gender” as input from user. If the
+user is male, give the message: Good Morning Sir. If the
+user is female, give the message: Good Morning Ma’am.
