7d217341e6b5bbd1e0cdb895d196729d2b3007eb
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-06 14:46:35 +0500

    Add solution to question 02 - gender prompt and conditional greeting

--- chapter-09-to-11-user-input-and-conditional-statement/question_02/app.js
+++ chapter-09-to-11-user-input-and-conditional-statement/question_02/app.js
@@ -2,4 +2,12 @@
 
 // 2. Write a program to take “gender” as input from user. If the user is male, give the message: Good Morning Sir. If the user is female, give the message: Good Morning Ma’am.
 
-var gender = 
+var gender = prompt("Enter Your Gender: ");
+
+if (gender == "male") {
+    console.log("Good Morning Sir");
+}
+
+if (gender == "female") {
+    console.log("Good Morning Ma’am");
+}
--- chapter-09-to-11-user-input-and-conditional-statement/question_03/app.js
+++ chapter-09-to-11-user-input-and-conditional-statement/question_03/app.js
@@ -1,1 +1,4 @@
 // Question 3 - Chapter 09-11 User Input and Conditional Statement
+
+// 3. Write a program to take input color of road traffic signal
+// from the user & show the message according to this table:
