c33f8a8f1512f36201c93d68ced02b02d7616bce
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-27 17:47:00 +0500

    add question 03 in chapter 35 to 38

---
+++ chapter-35-to-38-function/question-03/app.js
@@ -0,0 +1,8 @@
+function addTwoNum(num1, num2) {
+    var sum = num1+ num2;
+    return sum;
+}
+
+var userNum1 = +prompt("Enter Number 1");
+var userNum2 = +prompt("Enter Number 2");
+var userNumSum = addTwoNum(userNum1, userNum2);
