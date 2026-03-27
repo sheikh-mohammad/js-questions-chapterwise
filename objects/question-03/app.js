4bea61bf329376af48ea9748e37ee1ec63e4f555
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-27 17:52:29 +0500

    add question 03 in objects

---
+++ objects/question-03/app.js
@@ -0,0 +1,12 @@
+function Student(name, age, gender, rollNum, course, batch) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.rollNum = rollNum;
+    this.course = course;
+    this.batch = batch;
+}
+
+var student1 = new Student("Sheikh Mohammad", 14, "Male", 277210, "Web and Mobile Application Development", 18);
+var student2 = new Student("Ayesha", 15, "Female", 165973, "Web and Mobile Application Development", 19);
+var student3 = new Student("Mustafa", 16, "Male", 1828223, "Web and Mobile Application Development", 18);
