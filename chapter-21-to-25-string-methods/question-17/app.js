636897ebaec7ad25071f109ce7067241047a09da
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-27 17:10:41 +0500

    add question 17 in chapter 21 to 25

---
+++ chapter-21-to-25-string-methods/question-17/app.js
@@ -0,0 +1,4 @@
+var userInput = prompt("Enter User Input:");
+
+document.writeln(`User input: ${userInput} <br />`);
+document.writeln(`Last character of input: ${userInput[userInput.length - 1]}`);
