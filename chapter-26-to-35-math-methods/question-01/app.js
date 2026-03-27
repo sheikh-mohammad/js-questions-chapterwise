79aabe3cb208591ea433fa43d687cd22aa31ae01
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-27 17:16:29 +0500

    add question 01 in chapter 26 to 35

---
+++ chapter-26-to-35-math-methods/question-01/app.js
@@ -0,0 +1,10 @@
+var postiveInt = Number(prompt("Enter Positive Integer:"));
+
+if (postiveInt < 0) {
+    alert("Please Enter Postive Interger!");
+} else {
+    document.writeln(`number: ${postiveInt} <br />`);
+    document.writeln(`round off value: ${Math.round(postiveInt)} <br />`);
+    document.writeln(`floor value: ${Math.floor(postiveInt)} <br />`);
+    document.writeln(`ceil value: ${Math.ceil(postiveInt)}`);
+}
