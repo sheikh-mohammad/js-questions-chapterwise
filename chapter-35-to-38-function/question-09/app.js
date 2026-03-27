0e7e1eb282d03aa4e84edadf6df7d84bffd21ae1
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-27 17:49:50 +0500

    add question 09 in chapter 35 to 38

---
+++ chapter-35-to-38-function/question-09/app.js
@@ -0,0 +1,10 @@
+function areaTriangle(width, height) {
+    var A = width * height;
+    return A;
+}
+
+var area1 = areaTriangle(2, 5);
+
+var width = 2;
+var height = 5;
+var area2 = areaTriangle(width, height);
