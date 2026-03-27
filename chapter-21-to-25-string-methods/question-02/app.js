0f83c03d9a568e3054dc4a0e4e76ded615f720d8
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-27 16:51:43 +0500

    add question 02 in chapter 21 to 25

---
+++ chapter-21-to-25-string-methods/question-02/app.js
@@ -0,0 +1,4 @@
+var favMobile = prompt("Enter Your favorite mobile phone model.");
+
+document.writeln(`My favorite phone is: ${favMobile} <br />`);
+document.writeln(`Length of string: ${favMobile.length}`);
