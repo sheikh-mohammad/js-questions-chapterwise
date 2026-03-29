6e5433f1ef753a0c84cc7cf74a70357c4c732418
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-30 00:49:23 +0500

    Add question 08 in chapter 14 to 16

---
+++ chapter-14-to-16-arrays/question-08/app.js
@@ -0,0 +1,7 @@
+var studentNames = ["Michael", "John", "Tony"];
+var studentScores = [320, 230, 480];
+var totalMarks = 500
+
+document.writeln(`Scores of ${studentNames[0]} is ${studentScores[0]}. Percentage: ${(studentScores[0] / totalMarks) * 100}% <br />`);
+document.writeln(`Scores of ${studentNames[1]} is ${studentScores[1]}. Percentage: ${(studentScores[1] / totalMarks) * 100}% <br />`);
+document.writeln(`Scores of ${studentNames[2]} is ${studentScores[2]}. Percentage: ${(studentScores[2] / totalMarks) * 100}% <br />`);
