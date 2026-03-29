3c6b72e87beb0703f8e3068ee24e3d4af61bb786
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-30 00:28:32 +0500

    Add question 04 in chapter 06 to 09

---
+++ chapter-06-to-09-math-expressions/question-04/app.js
@@ -0,0 +1,12 @@
+var number = prompt("Enter Number to print its multiplication table:", 5);
+
+document.writeln(`${number} X 1 = ${number * 1} <br />`);
+document.writeln(`${number} X 2 = ${number * 2} <br />`);
+document.writeln(`${number} X 3 = ${number * 3} <br />`);
+document.writeln(`${number} X 4 = ${number * 4} <br />`);
+document.writeln(`${number} X 5 = ${number * 5} <br />`);
+document.writeln(`${number} X 6 = ${number * 6} <br />`);
+document.writeln(`${number} X 7 = ${number * 7} <br />`);
+document.writeln(`${number} X 8 = ${number * 8} <br />`);
+document.writeln(`${number} X 9 = ${number * 9} <br />`);
+document.writeln(`${number} X 10 = ${number * 10} <br />`);
