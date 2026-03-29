8fbbdecfb4a4c58ab4f14a6a21af822f821059d9
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-30 00:25:32 +0500

    Add question 03 in chapter 05

---
+++ chapter-05-math-expressions/question-03/app.js
@@ -0,0 +1,23 @@
+var variable;
+
+document.writeln(`Value after variable declaration is: ${variable} <br />`);
+
+variable = 5;
+
+document.writeln(`Initial value: ${variable} <br />`);
+
+variable++;
+
+document.writeln(`Value after increment is: ${variable} <br />`);
+
+variable += 7;
+
+document.writeln(`Value after addition is: ${variable} <br />`);
+
+variable--;
+
+document.writeln(`Value after decrement is: ${variable} <br />`);
+
+variable %= 3;
+
+document.writeln(`The remainder is: ${variable} <br />`);
