fdd726b0f52f8a521e2b5976b0f3e014e06232a9
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-30 00:50:20 +0500

    Add question 12 in chapter 14 to 16

---
+++ chapter-14-to-16-arrays/question-12/app.js
@@ -0,0 +1,6 @@
+var arr = ["This", "is", "my", "cat"];
+
+document.writeln("Array: <br />");
+document.writeln(`${arr} <br /> <br />`);
+document.writeln("String: <br />");
+document.writeln(arr.join(" "))
