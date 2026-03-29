2d7cdd2bcb595bffec51c9c6491ecf4234dbde0a
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-30 00:55:40 +0500

    Add question 08 in chapter 09 to 11

---
+++ chapter-09-to-11-user-input-and-conditional-statement/question-08/app.js
@@ -0,0 +1,9 @@
+// Question 8 - Chapter 09-11 User Input and Conditional Statement
+
+var number = +prompt("Enter a Number:");
+
+if (number % 3 === 0) {
+    alert(`${number} is Divisible by 3`);
+} else {
+    alert(`${number} is not Divisible by 3`);
+}
