6962306deb8dcc191c9fd70973ac9a6cab96d8f9
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-30 00:53:53 +0500

    Add question 01 in chapter 09 to 11

---
+++ chapter-09-to-11-user-input-and-conditional-statement/question-01/app.js
@@ -0,0 +1,9 @@
+// Question 1 - Chapter 09-11 User Input and Conditional Statement
+
+// // 1. Write a program to take “city” name as input from user. If user enters “Karachi”, welcome the user like this: “Welcome to city of lights”
+
+var city = prompt("Enter Your City Name: ");
+
+if (city == "Karachi") {
+    console.log("Welcome to city of lights");
+}
