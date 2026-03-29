8746e3c6f7fd1688c67753a3bb60092a76c899e9
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-30 00:55:50 +0500

    Add question 09 in chapter 09 to 11

---
+++ chapter-09-to-11-user-input-and-conditional-statement/question-09/app.js
@@ -0,0 +1,9 @@
+// Question 9 - Chapter 09-11 User Input and Conditional Statement
+
+var number = +prompt("Enter a number:");
+
+if (number % 2 === 0) {
+    alert(`${number} is a Even Number`);
+} else {
+    alert(`${number} is a Odd Number`);
+}
