4fe7318c5e3727d20196226fb2d571a270da22ab
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-30 00:26:42 +0500

    Add question 09 in chapter 05

---
+++ chapter-05-math-expressions/question-09/app.js
@@ -0,0 +1,9 @@
+var dollars = 279.67;
+var riyals = 74.49;
+
+var dollarQuant = 10;
+var riyalQuant = 25;
+
+var totalChange = (dollarQuant * dollars) + (riyalQuant * riyals);
+
+document.writeln(`Total Currency in PKR: ${totalChange}`);
