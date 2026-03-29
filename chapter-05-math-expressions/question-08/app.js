6d136ba7cf957650d4dd23321a1cd0238374519b
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-30 00:26:33 +0500

    Add question 08 in chapter 05

---
+++ chapter-05-math-expressions/question-08/app.js
@@ -0,0 +1,6 @@
+var totalMarks = 980;
+var obtainedMarks = 804;
+
+document.writeln(`Total marks: ${totalMarks} <br />`);
+document.writeln(`Marks obtained: ${obtainedMarks} <br />`);
+document.writeln(`Percentage: ${(obtainedMarks / totalMarks) * 100}%`)
