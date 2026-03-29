da5e7c91c136df0001cb2b282be8db1051be547b
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-30 00:50:57 +0500

    Add question 15 in chapter 14 to 16

---
+++ chapter-14-to-16-arrays/question-15/app.js
@@ -0,0 +1,12 @@
+var phoneManu = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
+
+document.writeln(`
+    <select>
+        <option>${phoneManu[0]}</option>
+        <option>${phoneManu[1]}</option>
+        <option>${phoneManu[2]}</option>
+        <option>${phoneManu[3]}</option>
+        <option>${phoneManu[4]}</option>
+        <option>${phoneManu[5]}</option>
+    </select>
+`);
