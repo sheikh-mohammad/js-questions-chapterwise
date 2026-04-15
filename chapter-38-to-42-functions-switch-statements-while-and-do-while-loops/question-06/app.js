be38e816b674645548b2b232657dcbf990bb58ea
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-04-15 19:33:08 +0500

    Add question 06 in chapter 38 to 42

--- chapter-38-to-42-functions-switch-statements-while-and-do-while-loops/question-06/app.js
+++ chapter-38-to-42-functions-switch-statements-while-and-do-while-loops/question-06/app.js
@@ -1,1 +1,23 @@
-// Question 06 - Functions, Switch Statements, While & Do-While Loops
+function deleteVowels(sentence) {
+    if (sentence.length > 25) {
+        console.log("Max length 25 chars allowed.");
+        return;
+    }
+    var lowerVowels = ["a", "e", "i", "o", "u"];
+    var upperVowels = ["A", "E", "I", "O", "U"];
+    var temp = [];
+    for (var i = 0; i < sentence.length; i++) {
+        if (lowerVowels.includes(sentence[i])) {
+            temp.push("");
+        } else if (upperVowels.includes(sentence[i])) {
+            temp.push("");
+        } else {
+            temp.push(sentence[i]);
+        }
+    }
+    var newSentence = temp.join("");
+    return newSentence;
+}
+
+console.log(deleteVowels("I am a boy. How are you. Do you want to play hide and seek"));
+console.log(deleteVowels("I am a boy."));
