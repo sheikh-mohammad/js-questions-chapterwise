8f66b4657d5c49bbe758d683255f8a8f4255f8c9
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-04-17 18:15:04 +0500

    Add question 07 in chapter 38 to 42

--- chapter-38-to-42-functions-switch-statements-while-and-do-while-loops/question-07/app.js
+++ chapter-38-to-42-functions-switch-statements-while-and-do-while-loops/question-07/app.js
@@ -1,3 +1,371 @@
+// aa ae ai ao au
+// ea ee ei eo eu
+// ia ie ii io iu
+// oa oe oi oo ou
+// ua ue ui uo uu
+
 function countPairVowels(sentence) {
-    
+    var loweredSentence = sentence.toLowerCase();
+    var count = 0;
+    for (var i = 0; i < sentence.length; i++) {
+        var first = sentence[i];
+        var second = sentence[i + 1];
+        switch (first) {
+            case "a":
+                switch (second) {
+                    case "a":
+                        count++;
+                        break;
+                    case "e":
+                        count++;
+                        break;
+                    case "i":
+                        count++;
+                        break;
+                    case "o":
+                        count++;
+                        break;
+                    case "u":
+                        count++;
+                        break;
+
+                    case "A":
+                        count++;
+                        break;
+                    case "E":
+                        count++;
+                        break;
+                    case "I":
+                        count++;
+                        break;
+                    case "O":
+                        count++;
+                        break;
+                    case "U":
+                        count++;
+                        break;
+                }
+                break;
+            case "e":
+                switch (second) {
+                    case "a":
+                        count++;
+                        break;
+                    case "e":
+                        count++;
+                        break;
+                    case "i":
+                        count++;
+                        break;
+                    case "o":
+                        count++;
+                        break;
+                    case "u":
+                        count++;
+                        break;
+
+                    case "A":
+                        count++;
+                        break;
+                    case "E":
+                        count++;
+                        break;
+                    case "I":
+                        count++;
+                        break;
+                    case "O":
+                        count++;
+                        break;
+                    case "U":
+                        count++;
+                        break;
+                }
+                break;
+            case "i":
+                switch (second) {
+                    case "a":
+                        count++;
+                        break;
+                    case "e":
+                        count++;
+                        break;
+                    case "i":
+                        count++;
+                        break;
+                    case "o":
+                        count++;
+                        break;
+                    case "u":
+                        count++;
+                        break;
+
+                    case "A":
+                        count++;
+                        break;
+                    case "E":
+                        count++;
+                        break;
+                    case "I":
+                        count++;
+                        break;
+                    case "O":
+                        count++;
+                        break;
+                    case "U":
+                        count++;
+                        break;
+                }
+                break;
+            case "o":
+                switch (second) {
+                    case "a":
+                        count++;
+                        break;
+                    case "e":
+                        count++;
+                        break;
+                    case "i":
+                        count++;
+                        break;
+                    case "o":
+                        count++;
+                        break;
+                    case "u":
+                        count++;
+                        break;
+
+                    case "A":
+                        count++;
+                        break;
+                    case "E":
+                        count++;
+                        break;
+                    case "I":
+                        count++;
+                        break;
+                    case "O":
+                        count++;
+                        break;
+                    case "U":
+                        count++;
+                        break;
+                }
+                break;
+            case "u":
+                switch (second) {
+                    case "a":
+                        count++;
+                        break;
+                    case "e":
+                        count++;
+                        break;
+                    case "i":
+                        count++;
+                        break;
+                    case "o":
+                        count++;
+                        break;
+                    case "u":
+                        count++;
+                        break;
+
+                    case "A":
+                        count++;
+                        break;
+                    case "E":
+                        count++;
+                        break;
+                    case "I":
+                        count++;
+                        break;
+                    case "O":
+                        count++;
+                        break;
+                    case "U":
+                        count++;
+                        break;
+                }
+                break;
+
+            case "A":
+                switch (second) {
+                    case "a":
+                        count++;
+                        break;
+                    case "e":
+                        count++;
+                        break;
+                    case "i":
+                        count++;
+                        break;
+                    case "o":
+                        count++;
+                        break;
+                    case "u":
+                        count++;
+                        break;
+
+                    case "A":
+                        count++;
+                        break;
+                    case "E":
+                        count++;
+                        break;
+                    case "I":
+                        count++;
+                        break;
+                    case "O":
+                        count++;
+                        break;
+                    case "U":
+                        count++;
+                        break;
+                }
+                break;
+            case "E":
+                switch (second) {
+                    case "a":
+                        count++;
+                        break;
+                    case "e":
+                        count++;
+                        break;
+                    case "i":
+                        count++;
+                        break;
+                    case "o":
+                        count++;
+                        break;
+                    case "u":
+                        count++;
+                        break;
+
+                    case "A":
+                        count++;
+                        break;
+                    case "E":
+                        count++;
+                        break;
+                    case "I":
+                        count++;
+                        break;
+                    case "O":
+                        count++;
+                        break;
+                    case "U":
+                        count++;
+                        break;
+                }
+                break;
+            case "I":
+                switch (second) {
+                    case "a":
+                        count++;
+                        break;
+                    case "e":
+                        count++;
+                        break;
+                    case "i":
+                        count++;
+                        break;
+                    case "o":
+                        count++;
+                        break;
+                    case "u":
+                        count++;
+                        break;
+
+                    case "A":
+                        count++;
+                        break;
+                    case "E":
+                        count++;
+                        break;
+                    case "I":
+                        count++;
+                        break;
+                    case "O":
+                        count++;
+                        break;
+                    case "U":
+                        count++;
+                        break;
+                }
+                break;
+            case "O":
+                switch (second) {
+                    case "a":
+                        count++;
+                        break;
+                    case "e":
+                        count++;
+                        break;
+                    case "i":
+                        count++;
+                        break;
+                    case "o":
+                        count++;
+                        break;
+                    case "u":
+                        count++;
+                        break;
+
+                    case "A":
+                        count++;
+                        break;
+                    case "E":
+                        count++;
+                        break;
+                    case "I":
+                        count++;
+                        break;
+                    case "O":
+                        count++;
+                        break;
+                    case "U":
+                        count++;
+                        break;
+                }
+                break;
+            case "U":
+                switch (second) {
+                    case "a":
+                        count++;
+                        break;
+                    case "e":
+                        count++;
+                        break;
+                    case "i":
+                        count++;
+                        break;
+                    case "o":
+                        count++;
+                        break;
+                    case "u":
+                        count++;
+                        break;
+
+                    case "A":
+                        count++;
+                        break;
+                    case "E":
+                        count++;
+                        break;
+                    case "I":
+                        count++;
+                        break;
+                    case "O":
+                        count++;
+                        break;
+                    case "U":
+                        count++;
+                        break;
+                }
+                break;
+        }
+    }
+
+    return count;
 }
+
+console.log("Pleases read this application and give me gratuity", countPairVowels("Pleases read this application and give me gratuity"));
--- chapter-38-to-42-functions-switch-statements-while-and-do-while-loops/question-09/app.js
+++
@@ -1,1 +0,0 @@
-// Question 09 - Functions, Switch Statements, While & Do-While Loops
